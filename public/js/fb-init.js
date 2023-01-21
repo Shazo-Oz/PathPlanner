32b4fb8c3f250d460a0ec72b12de160e6066ef80
Daniel280200 <danieler@post.jce.ac.il>
2023-01-21 11:39:43 +0200

    fb-env

--- public/js/fb-init.js
+++ public/js/fb-init.js
@@ -1,8 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-import firebaseConfig from "./fb-env.js"
+import fbConfig from "./fb-env.js"
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js'
 import { getFirestore, doc } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js'
 
+const firebaseConfig=JSON.parse(atob(fbConfig.key))
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth()
 export const db = getFirestore(app);
